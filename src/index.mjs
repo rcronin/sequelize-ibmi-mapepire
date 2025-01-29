a3fd3485ab48503d4f7008d0914af7d68a26b643
rcronin <rcronin@daytonfreight.com>
2025-01-29 16:09:26 -0500

    More tsconfig tweaks

--- src/index.mjs
+++
@@ -1,7 +0,0 @@
-import Pkg from './index.js';
-
-export const IBMiConnectionManager = Pkg.IBMiConnectionManager;
-export const IBMiDialect = Pkg.IBMiDialect;
-export const IBMiQueryGenerator = Pkg.IBMiQueryGenerator;
-export const IBMiQueryInterface = Pkg.IBMiQueryInterface;
-export const IBMiQuery = Pkg.IBMiQuery;
