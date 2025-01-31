6cf93c1ca81e46205e1ba1f1bc1937f3d06f0fe4
rcronin <rcronin@daytonfreight.com>
2025-01-31 08:58:50 -0500

    feat: add NestJS example with initial setup and configuration [skip actions]

--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -1,46 +1,9 @@
-import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from '@sequelize/core';
-import { Attribute, PrimaryKey, Table } from '@sequelize/core/decorators-legacy';
+import { Sequelize } from '@sequelize/core';
 import { IBMiDialect } from '@rcronin/sequelize-ibmi-mapepire';
 import 'dotenv/config'
+import { DepartmentModel } from './models/test.model';
 
-@Table({
-  freezeTableName: true,
-  timestamps: false,
-  tableName: 'DEPARTMENT', // whatever the actual IBM i table is called
-  schema: 'SAMPLE' // whatever the actual IBM i schema is called
-})
-export class DepartmentModel extends Model<InferAttributes<DepartmentModel>, InferCreationAttributes<DepartmentModel>> {
-  @Attribute({
-    type: DataTypes.STRING,
-    columnName: 'DEPTNO'
-  })
-  @PrimaryKey()
-  declare number: string;
 
-  @Attribute({
-    type: DataTypes.STRING,
-    columnName: 'DEPTNAME'
-  })
-  declare name: string;
-
-  @Attribute({
-    type: DataTypes.STRING,
-    columnName: 'MGRNO'
-  })
-  declare managerNumber: string;
-
-  @Attribute({
-    type: DataTypes.STRING,
-    columnName: 'ADMRDEPT'
-  })
-  declare parent: string;
-
-  @Attribute({
-    type: DataTypes.STRING,
-    columnName: 'LOCATION'
-  })
-  declare location: string;
-}
 
 async function run() {
   const sequelize = new Sequelize({
